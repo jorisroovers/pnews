fcf891fba419563655bfd97a650eabed444b72d5
Joris Roovers <joris.roovers@gmail.com>
2015-11-19 16:40:57 +0100

    Poller stores reddit stores in MongoDB
    We use the Mongoose ODM library to store reddit stories in MongoDB.

--- poller/app.js
+++ poller/app.js
@@ -1,6 +1,6 @@
-var Q = require("q");
-var request = require('request');
-var MongoClient = require('mongodb').MongoClient
+var Q = require("q"),
+    request = require('request'),
+    mongoose = require('mongoose');
 
 var VERSION = "0.0.1"
 var USER_AGENT = " pnews-poller:v" + VERSION;
@@ -12,7 +12,10 @@ var URLS = {
         'r_programming_top': "https://oauth.reddit.com/r/programming/top"
     }
 }
-var ACCESS_TOKEN = "dw"; // For now, we use a global var to store the access token
+
+// TODO(jroovers): For now, we use some global vars to store some stuff, let's change this soon
+var ACCESS_TOKEN = "bogus"; // this MUST be set to a bogus token initially, otherwise the API returns HTML
+var RedditPost = null;
 
 // Simple way to get reddit access token as per: https://github.com/reddit/reddit/wiki/OAuth2-Quick-Start-Example
 function get_access_token() {
@@ -40,8 +43,8 @@ function get_access_token() {
 
 // Polls /r/programming for the top stories
 function poll() {
-    console.info("Polling reddit for latest programming news")
-    options = {
+    console.info("Polling reddit for latest programming news...")
+    var options = {
         'url': URLS.reddit.r_programming_top,
         'auth': {'bearer': ACCESS_TOKEN},
         'headers': {
@@ -49,10 +52,9 @@ function poll() {
         },
         'json': true,
     }
-    // try to get
     request.get(options, function (error, response, body) {
         if (error) {
-            console.error("An error occured while trying to poll reddit:", error);
+            console.error("An error occurred while trying to poll reddit:", error);
             return;
         }
         // if we are unauthenticated, then get a new token first and redo poll
@@ -61,10 +63,21 @@ function poll() {
             get_access_token().then(poll);
         }
         else if (response.statusCode == 200) {
-            body.data.children.forEach(function (post) {
-                console.info("[%s] %s", post.data.score, post.data.title);
-                console.info("\t", post.data.permalink);
-                console.info("\t", post.data.url);
+            // Unfortunately, mongoose doesn't support bulk save yet, so we'll need to save the posts one by one:
+            //https://github.com/Automattic/mongoose/issues/723
+            var savedCount = 0;
+            body.data.children.forEach(function (postData) {
+                var post = new RedditPost(postData);
+                var query = {'data.id': postData.data.id};
+                RedditPost.update(query, postData, {upsert: true}, function (err) {
+                    savedCount++;
+                    if (err) {
+                        console.warn("Failed saving post: [%s] %s", post.data.title, post.data.title);
+                    }
+                    if (savedCount == body.data.children.length) {
+                        console.info("Saved posts to DB.")
+                    }
+                });
             });
         }
         else {
@@ -89,18 +102,19 @@ function init() {
         console.error("pnews-poller not correctly initialized, exiting.");
         process.exit(1)
     }
+
+    // Set connection to keep-alive so that we don't need to worry about losing the db connection:
+    // http://mongoosejs.com/docs/connections.html
+    var options = {
+        server: {socketOptions: {keepAlive: 1}},
+        replset: {socketOptions: {keepAlive: 1}}
+    };
+    mongoose.connect(process.env.MONGO_DB_URL, options);
+    // set strict: false so that we can just insert reddit posts without having to define the schema here
+    var RedditPostSchema = mongoose.Schema({data: {id: {type: [String], index: true}}}, {strict: false});
+    RedditPost = mongoose.model('RedditPost', RedditPostSchema);
+
 }
-
-// Use connect method to connect to the Server
-MongoClient.connect(process.env.MONGO_DB_URL, function (err, db) {
-    if (err) {
-        console.error("ERROR", err);
-        return
-    }
-    console.log("Connected correctly to server");
-
-    db.close();
-});
 
 //////////////////
 // ENTRY POINT
